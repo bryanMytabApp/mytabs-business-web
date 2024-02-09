1ea02be498aab108328f6ccfb68087a219a31b70
Eduardo Escamilla <eduardo.escamillasal@gmail.com>
2024-02-09 13:11:21 -0600

    MTB-2 - added password validators

--- client/src/components/MTBInputValidator/MTBInputValidator.jsx
+++ client/src/components/MTBInputValidator/MTBInputValidator.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import bulletSelector from "../../assets/atoms/bulletSelector.svg";
 import "./MTBInputValidator.css";
 
-const MTBInputValidator = ({textRequirement}) => {
+const MTBInputValidator = ({textRequirement, isValid}) => {
+  const textStyle = isValid ? {color: "green"} : {};
+  const iconClass = isValid ? "green-filter" : "";
+
   return (
-    <div style={{display: "flex", flexDirection: "row"}}>
-      <img src={bulletSelector} alt='bulletSelector' />{" "}
-      <div className='input-validator-text-req'>{textRequirement}</div>
+    <div style={{display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
+      <img src={bulletSelector} alt='bulletSelector' className={iconClass} />
+      {"   "}
+      <div className='input-validator-text-req' style={textStyle}>
+        {textRequirement}
+      </div>
     </div>
   );
 };
