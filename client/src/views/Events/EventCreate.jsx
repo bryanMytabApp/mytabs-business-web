26710ae2f0b94d8c565e0958cc13ba18303affa6
Daniboy2802 <dv.santibanez@gmail.com>
2024-04-09 17:11:42 -0600

    MTB-76 - tickets

--- client/src/views/Events/EventCreate.jsx
+++ client/src/views/Events/EventCreate.jsx
@@ -12,33 +12,52 @@ import moment from 'moment'
 import { useNavigate } from "react-router-dom";
 import selectIcon from "../../assets/atoms/selectIcon.svg";
 import selectIconActive from "../../assets/atoms/selectIconActive.svg";
-import { MTBDropZone, MTBInput, MTBSelector } from "../../components";
+import { MTBDropZone, MTBSelector } from "../../components";
 import { State, City } from 'country-state-city';
-import { createEvent, getEventsByUserId, getPresignedUrlForEvent } from "../../services/eventService";
+import { createEvent, getPresignedUrlForEvent } from "../../services/eventService";
 import axios from "axios";
 
 const eventTypes = [
   {
     name: 'Event',
     icon: 'event_available',
-    type: "0"
+    type: "0",
   },
 ]
 
-const cityList = [
-  {value: 0, name: "Austin", color: "#fff"},
-  {value: 1, name: "Dallas", color: "#fff"},
-  {value: 2, name: "Houston", color: "#fff"},
-  {value: 3, name: "Los Angeles", color: "#fff"},
-  {value: 3, name: "New Orleans", color: "#fff"},
+const ticketingOptions = [
+  { 
+    value: 0, 
+    name: "External link",
+  },
+  { 
+    value: 1, 
+    name: "Free",
+  },
+  { 
+    value: 2, 
+    name: "RSVP",
+  },
 ];
+
 let userId
+const countryCode = 'US';
+const baseTicket = {
+  option: 'External link',
+  type: '',
+  error: false
+}
+
 const EventCreate = () => {
   const [selectedItem, setSelectedItem] = useState("")
   const [states, setStates] = useState([])
   const [cities, setCities] = useState([])
   const [step, setStep] = useState(0)
   const [uploadedImage, setUploadedImage] = useState(null)
+  const [addressOption, setAddressOption] = useState(0)
+  const [tickets, setTickets] = useState([baseTicket])
+  const [ticketSelectedIndex, setTicketSelectedIndex] = useState(0)
+  
   const [item, setItem] = useState({
     name: '',
     city: '',
@@ -50,14 +69,19 @@ const EventCreate = () => {
     address2: '',
     zipCode: '',
   })
-  const [addressOption, setAddressOption] = useState(0)
+  
   const navigation = useNavigate();
+  
   const handleGoBack = () => navigation("/admin/my-events")
+  
   const createMultipleClasses = (classes = []) => classes.filter(cl => cl).join(' ');
-  const countryCode = 'US';
+  
   const handleContinue = (nextStep, lastStep = false) => {
     if(!lastStep) {
       setStep(nextStep)
+      return
+    }
+    if(!ticketsValidated()) {
       return
     }
     _createEvent()
@@ -68,7 +92,7 @@ const EventCreate = () => {
     itemCopy.startDate = moment(itemCopy.startDate).toString()
     itemCopy.endDate = moment(itemCopy.endDate).toString()
     itemCopy.userId = userId
-
+    itemCopy.tickets = tickets
     let data
     try {
       let res = await createEvent(itemCopy)
@@ -101,20 +125,6 @@ const EventCreate = () => {
       handleGoBack()
     }
     handleGoBack()
-    // this.convertToBase64(file)
-    // function convertToBase64(file) {
-    //   let reader = new FileReader()
-    //   reader.onload = async (e) => {
-    //     this.file = e.target.result
-    //   }
-    //   reader.readAsDataURL(file)
-    // },
-    // if(!this.file || !this.folder) 
-    //   return
-    // const base64Response = await fetch(this.file);
-    // const blob = await base64Response.blob();
-    // if(!blob) 
-    //   return
   }
 
   const handleItemChange = (attr, value) => {
@@ -160,10 +170,8 @@ const EventCreate = () => {
     }
     let selectedState = states.find(state => state.name === item.state)
     let availableCities = City.getCitiesOfState(countryCode, selectedState.isoCode)
-    console.log("🚀 ~ useEffect ~ availableCities:", availableCities)
     setCities(availableCities)
-    console.log("🚀 ~ useEffect ~ currentState:", selectedState)
-  }, [item.state]);
+  }, [item.state])
 
   const parseJwt = (token) => {
     const base64Url = token.split(".")[1];
@@ -180,6 +188,62 @@ const EventCreate = () => {
     return JSON.parse(jsonPayload)["custom:user_id"];
   };
 
+  const changeTicketSelectedAttr = (attr, value) => {
+    let selectedTicketCopy = tickets[ticketSelectedIndex]
+    selectedTicketCopy = {
+      ...selectedTicketCopy,
+      [attr]: value
+    }
+    selectedTicketCopy = validateTicket(selectedTicketCopy)
+    let ticketsCopy = JSON.parse(JSON.stringify(tickets))
+    ticketsCopy[ticketSelectedIndex] = selectedTicketCopy
+    setTickets(ticketsCopy)
+  }
+
+  const addNewTicket = () => {
+    let ticketsCopy = JSON.parse(JSON.stringify(tickets))
+    ticketsCopy.push(Object.assign({}, baseTicket))
+    
+    setTickets(ticketsCopy)
+    setTicketSelectedIndex(ticketsCopy.length - 1)
+  }
+
+  const deleteTicket = (ticketIndex) => {
+    if(tickets.length === 1) {
+      return
+    } 
+    let ticketsCopy = JSON.parse(JSON.stringify(tickets))
+    ticketsCopy.splice(ticketIndex, 1)
+    setTickets(ticketsCopy)
+    setTicketSelectedIndex(0)
+  } 
+
+  const validateTicket = (ticket = {}, index) => {
+    let error = false
+    if(ticket.option === 'External link') {
+      if((!ticket.link1 && !ticket.link2 && !ticket.link3) || !ticket.type) {
+        error = true
+      }
+    } else {
+      if(!ticket.type) {
+        error = true
+      }
+    }
+    return {
+      ...ticket,
+      error
+    }
+  }
+
+  const ticketsValidated = () => {
+    let ticketsCopy = JSON.parse(JSON.stringify(tickets))
+    ticketsCopy = ticketsCopy.map(ticket => validateTicket(ticket))
+    setTickets(ticketsCopy)
+    if(ticketsCopy.some(t => t.error)) {
+      return false
+    }
+    return true
+  }
 
  return (
   <div className={styles.view}>
@@ -550,75 +614,6 @@ const EventCreate = () => {
                 </span>              
               )}
             </div>
-            {/* <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%', marginBottom: '30px', flexDirection: 'column' }}>
-              <span style={{ width: '100%' }}>
-                <div className={styles.title}>
-                  Add a description or relevant information
-                </div>
-                <div className={styles.inputContainer} style={{ width: '100%', marginBottom: '10px' }}>
-                  <textarea
-                    className={styles.input}
-                    style={{ width: '100%', height: '80px', resize: 'none' }}
-                    cols="20" rows="1"
-                    value={item.description}
-                    placeholder="Description 140 characters"
-                    onBlur={() => {}}
-                    onChange={(e) => handleItemChange('description',e.target.value)}
-                  />
-                </div>
-              </span>
-              <span style={{ width: '100%' }}>
-                <div className={styles.inputContainer} style={{ marginBottom: '10px' }}>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    value={item.address1}
-                    placeholder="Address1"
-                    onBlur={() => {}}
-                    onChange={(e) => handleItemChange('address1',e.target.value)}
-                  />
-                </div>
-                <div style={{ display: 'flex', width: '100%', justifyContent: 'space-between' }}>
-                  <div className={styles.inputContainer} style={{ width: '45%' }}>
-                    <input
-                      className={styles.input}
-                      type="text"
-                      value={item.zipCode}
-                      placeholder="Zip Code"
-                      onBlur={() => {}}
-                      onChange={(e) => handleItemChange('zipCode',e.target.value)}
-                    />
-                  </div>
-                  <span>
-                    <div className={styles.title}>
-                      Where is it located?  
-                    </div>
-                    <MTBSelector
-                      onBlur={() => ("city")}
-                      name={"city"}
-                      placeholder='City'
-                      autoComplete='city'
-                      value={item.city}
-                      itemName={"name"}
-                      itemValue={"name"}
-                      options={cityList}
-                      onChange={(selected, fieldName) => {
-                        handleItemChange('city', selected);
-                      }}
-                      styles={{
-                        display: 'flex',
-                        background: '#FCFCFC',
-                        borderRadius: '10px',
-                        boxShadow: '0px 4.679279327392578px 9.358558654785156px 0px #32324702',
-                        boxShadow: '0px 4.679279327392578px 4.679279327392578px 0px #00000014',
-                        width: '240px',
-                        height: '28px',
-                      }}
-                    />
-                  </span>
-                </div>
-              </span>
-            </div> */}
             <button
               disabled={disabledButtonOnStepThree()}
               className={createMultipleClasses([styles.baseButton, styles.createEventButton, disabledButtonOnStepThree() ? styles.disabled : ''])}
@@ -655,7 +650,202 @@ const EventCreate = () => {
             <button
               disabled={!uploadedImage}
               className={createMultipleClasses([styles.baseButton, styles.createEventButton, !uploadedImage ? styles.disabled : ''])}
-              onClick={() => handleContinue(3, true)}
+              onClick={() => handleContinue(4, false)}
+            >
+              Next
+            </button>
+          </div>
+        )}
+        {step === 4 && (
+          <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              width: '100%',
+              height: '90%',
+            }}
+          >
+            <div style={{ display: 'flex', justifyContent: 'space-around', width: '100%', marginBottom: '30px', height: '100%', alignContent: 'space-between' }}>
+              <span className={styles['tickets-viewer-container']}>
+                <div className={styles['ticket-list-container']}>
+                  {tickets.map((ticket, index) => (
+                    <div
+                      className={styles['individual-ticket-container']}
+                    >
+                      <span
+                        style={{
+                          width: '100%',
+                          display: 'flex',
+                          cursor: 'pointer',
+                          alignItems: 'center'
+                        }}
+                        onClick={() => setTicketSelectedIndex(index)}
+                      >
+                        <div
+                          style={{
+                            width: '20px',
+                            height: '20px',
+                            background: 'white',
+                            borderRadius: '50%',
+                            cursor: 'pointer',
+                            boxShadow: '0px 0px 0px 4px #98A2B324',
+                            marginRight: '10px',
+                          }}
+                        >
+                          <div>
+                            <img
+                              style={{justifySelf: "flex-end"}}
+                              src={ticketSelectedIndex === index ? selectIconActive : selectIcon}
+                              alt='bullet'
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <div
+                            className={
+                              createMultipleClasses([
+                                styles['outfit-font'],
+                              ])
+                            }
+                            style={{
+                              fontWeight: 800,
+                              color: ticket.error ? 'red' : ticketSelectedIndex === index ? '#00AAD6' : '#514F4F'
+                            }}
+                          >
+                            Ticket {index <= 8 ? `0${index + 1}` : index + 1}
+                          </div>
+                          <div className={createMultipleClasses([styles['outfit-font']])}>
+                            {ticket.option}
+                          </div>
+                        </div>
+                      </span>
+                      <div className={styles['delete-icon']} onClick={() => deleteTicket(index)}>
+                        <span className={createMultipleClasses([
+                          'material-symbols-outlined',
+                          tickets.length === 1 ? styles['disabled'] : ''
+                        ])}>
+                          delete
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div
+                  className={createMultipleClasses([styles['add-another-ticket-container'], styles['primary-color']])}
+                  onClick={addNewTicket}
+                >
+                  <span className={styles['add-another-ticket-text']}>Add another ticketing option?</span >
+                  <span class="material-symbols-outlined">
+                    add
+                  </span>
+                </div>
+              </span>
+              <span style={{ width: '50%' }}>
+                <span>
+                  <div className={styles.title} style={{ marginBottom: 0, fontWeight: 700 }}>
+                    Select ticketing options
+                  </div>
+                  <MTBSelector
+                    onBlur={() => ("name")}
+                    name={"name"}
+                    placeholder='Type'
+                    value={tickets[ticketSelectedIndex].option}
+                    itemName={"name"}
+                    itemValue={"name"}
+                    options={ticketingOptions}
+                    onChange={(selected, fieldName) => {
+                      changeTicketSelectedAttr('option', selected);
+                    }}
+                    styles={{
+                      display: 'flex',
+                      background: '#FCFCFC',
+                      borderRadius: '10px',
+                      boxShadow: '0px 4.679279327392578px 9.358558654785156px 0px #32324702',
+                      boxShadow: '0px 4.679279327392578px 4.679279327392578px 0px #00000014',
+                      width: '40%',
+                      height: '18px',
+                    }}
+                  />
+                </span>
+                <span>
+                  <div style={{ marginBottom: '10px', marginTop: '10px' }}>
+                    Type of ticket
+                  </div>
+                  <div className={styles.inputContainer} style={{ width: '100%', padding: '5px 10px' }}>
+                    <input
+                      className={styles.input}
+                      type="text"
+                      value={tickets[ticketSelectedIndex].type}
+                      placeholder="Type your ‘Type of ticket’"
+                      onBlur={() => {}}
+                      onChange={(e) => changeTicketSelectedAttr('type',e.target.value)}
+                    />
+                  </div>
+                  {tickets[ticketSelectedIndex]?.option === 'External link' && (
+                    <div>
+                      <div className={styles.title} style={{ marginBottom: '7px', fontWeight: 700 }}>
+                        Add external links
+                      </div>
+                      <div className={styles.inputContainer} style={{ width: '100%', marginBottom: '10px', padding: '5px 10px' }}>
+                        <input
+                          className={styles.input}
+                          type="text"
+                          value={tickets[ticketSelectedIndex].link1 || ''}
+                          placeholder="Link 1"
+                          onBlur={() => {}}
+                          onChange={(e) => changeTicketSelectedAttr('link1',e.target.value)}
+                        />
+                      </div>
+                      <div className={styles.inputContainer} style={{ width: '100%', marginBottom: '10px', padding: '5px 10px' }}>
+                        <input
+                          className={styles.input}
+                          type="text"
+                          value={tickets[ticketSelectedIndex].link2 || ''}
+                          placeholder="Link 2"
+                          onBlur={() => {}}
+                          onChange={(e) => changeTicketSelectedAttr('link2',e.target.value)}
+                        />
+                      </div>
+                      <div className={styles.inputContainer} style={{ width: '100%', padding: '5px 10px' }}>
+                        <input
+                          className={styles.input}
+                          type="text"
+                          value={tickets[ticketSelectedIndex].link3 || ''}
+                          placeholder="Link 3"
+                          onBlur={() => {}}
+                          onChange={(e) => changeTicketSelectedAttr('link3',e.target.value)}
+                        />
+                      </div>
+                    </div>
+                  )}
+                  {(tickets[ticketSelectedIndex]?.option === 'Free' || tickets[ticketSelectedIndex].option === 'RSVP') && (
+                    <div>
+                      <span style={{ width: '100%' }}>
+                        <div className={styles.title} style={{ fontWeight: 700, marginBottom: 0,marginTop: '15px' }}>
+                          Add additional information
+                        </div>
+                        <div className={styles.inputContainer} style={{ width: '100%', marginBottom: '10px', padding: '5px 10px' }}>
+                          <textarea
+                            className={styles.input}
+                            style={{ width: '100%', height: '80px', resize: 'none' }}
+                            cols="20" rows="1"
+                            value={tickets[ticketSelectedIndex].description || ''}
+                            placeholder="Type your additional information..."
+                            onBlur={() => {}}
+                            onChange={(e) => changeTicketSelectedAttr('description',e.target.value)}
+                          />
+                        </div>
+                      </span>
+                    </div>
+                  )}
+                </span>
+              </span>
+            </div>
+            <button
+              className={createMultipleClasses([styles.baseButton, styles.createEventButton])}
+              style={{ marginTop: '0px' }}
+              onClick={() => handleContinue(4, true)}
             >
               Next
             </button>
